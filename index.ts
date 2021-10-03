0a84e4b4143b1cdf55b0b205a4b136eb75b9be9e
Magicoo51889 <jackot11f@gmail.com>
2021-10-04 00:02:38 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
-import PromptSync from "prompt-sync";
+import * as PromptSync from "prompt-sync";
 const prompt = PromptSync();
 
-let name: string = prompt("Enter your name: ")
-let strAge: string = prompt("Enter your age")
-let Age: number = parseInt(strAge);
+function nameAge(){
+    let name: string = prompt("Enter your name: ")
+    let strAge: string = prompt("Enter your age")
+    let Age: number = parseInt(strAge);
 
-console.log("Hello ${name}, you are ${Age} years old.")
+    console.log("Hello ${name}, you are ${Age} years old.")
+}
+
+nameAge();
+   
