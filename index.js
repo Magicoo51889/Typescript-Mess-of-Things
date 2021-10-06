2490e71d269a45e6efe88dc182c769097c2fb234
Magicoo51889 <61296373+Magicoo51889@users.noreply.github.com>
2021-10-06 10:21:24 +0100

    Removed '=' from console.log

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 "use strict";
 exports.__esModule = true;
-var prompt_sync_1 = require("prompt-sync");
-var prompt = (0, prompt_sync_1["default"])();
+var promptSync = require("prompt-sync");
+var prompt = promptSync();
 function workOutPI() {
     var PI = 3.14159265359;
     var radiusStr = prompt("What is the radius of your circle? ");
     var radius = parseInt(radiusStr);
     var diameter = radius * 2;
     var area = PI * (Math.pow(radius, 2));
-    console.log = ("Your circle has a radius of ${radius}, a diameter of ${diameter} and an area of ${area}");
+    console.log("Your circle has a radius of ${radius}, a diameter of ${diameter} and an area of ${area}");
 }
 workOutPI();
--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
-import promptSync from "prompt-sync";
+import * as promptSync from "prompt-sync";
 const prompt = promptSync();
 
 function workOutPI(){
-    let PI: number = 3.14159265359
+    const PI: number = 3.14159265359
     let radiusStr: string = prompt("What is the radius of your circle? ")
     let radius: number = parseInt(radiusStr);
 
     let diameter: number = radius * 2
     let area: number = PI * (radius ** 2)
 
-    console.log = ("Your circle has a radius of ${radius}, a diameter of ${diameter} and an area of ${area}")
+    console.log("Your circle has a radius of ${radius}, a diameter of ${diameter} and an area of ${area}");
 }
 
 workOutPI();
