cd5d355cfcb02bf58940e6e464bde719f7f9d3bd
Magicoo51889 <61296373+Magicoo51889@users.noreply.github.com>
2021-10-08 10:05:18 +0100

    Added which file to run in index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import * as promptSync from 'prompt-sync'
+const prompt = promptSync();
+
 import { makeSentence } from "./utils/forFirstTask";
 import { flowchart1 } from "./utils/Flowcharts";
 import { workOutPI } from "./utils/piConverter";
@@ -6,10 +9,31 @@ import { animalSelection } from "./utils/selectionProgram";
 import { vegetableSelection } from "./utils/selectionProgram";
 import { timesTable } from './utils/timesTable';
 
-makeSentence();
-flowchart1();
-workOutPI();
-printBill();
-animalSelection();
-vegetableSelection();
-timesTable();
+let option = prompt("Select program to run: ")
+
+
+if (option = "makeSentence") {
+    console.log("...Running makeSentence...")
+    makeSentence();
+} else if (option = "flowchart") {
+    console.log("...Running flowchart1...")
+    flowchart1();
+} else if (option = "workoutPi") {
+    console.log("...Running workoutPi...")
+    workOutPI();
+} else if (option = "printBill") {
+    console.log("...Running printBill...")
+    printBill();
+} else if (option = "animalSelection") {
+    console.log("...Running animalSelection...")
+    animalSelection();
+} else if (option = "vegetableSelection") {
+    console.log("...Running vegetableSelection")
+    vegetableSelection();
+} else if (option = "timesTable") {
+    console.log("...Running timesTable...")
+    timesTable();
+}
+
+
+
