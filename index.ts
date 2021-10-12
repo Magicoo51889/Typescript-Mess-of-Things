fec90a71e2a38d23920ac02fdf213ca1381e4bea
Magicoo51889 <61296373+Magicoo51889@users.noreply.github.com>
2021-10-12 13:13:40 +0100

    Added rainbow list program

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as promptSync from 'prompt-sync'
 const prompt = promptSync();
-
+import './utils'
 import { makeSentence } from "./utils/forFirstTask";
 import { flowchart1 } from "./utils/Flowcharts";
 import { workOutPI } from "./utils/piConverter";
@@ -8,6 +8,7 @@ import { printBill } from "./utils/splitBill";
 import { animalSelection } from "./utils/selectionProgram";
 import { vegetableSelection } from "./utils/selectionProgram";
 import { timesTable } from './utils/timesTable';
+import { rainbowList } from './utils/colourLists';
 
 console.log("Options: makesentece; flowchart; workoutpi; printbill; animlaselection; vegetableselection; timestable")
 let optionStr:string = prompt("Select program to run: ")
@@ -35,6 +36,9 @@ if (option === "makesentence") {
 } else if (option === "timestable") {
     console.log("...Running timesTable...")
     timesTable();
+} else if (option === "colourlists") {
+    console.log("...Running colourLists...")
+    rainbowList();
 } else {
     console.log("...File not found...")
 }
---
+++ utils/colourLists.ts
@@ -0,0 +1,11 @@
+export function rainbowList() {
+    const colours: string[] = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+    console.log("...Printing the colours in a rainbow...")
+    
+    for (let i=0; i++;) {
+        console.log(colours[i]);
+    }
+
+    console.log("Finsihed")
+}
