0366bf31301233e721e4829b01bf733f1f75d981
Magicoo51889 <61296373+Magicoo51889@users.noreply.github.com>
2021-10-21 09:25:53 +0100

    Update ksp_man_Jeb.ts

--- typescript/OOP/ksp_man_Jeb.ts
+++ typescript/OOP/ksp_man_Jeb.ts
@@ -18,6 +18,35 @@ abstract class ksp_man {
 class Jeb extends ksp_man {
     talk() {
         this.name = "Jebediah"
+        this.age = 20
+        this.profession = "pilot"
+        this.badS = true
+    }
+}
+
+class Bill extends ksp_man {
+    talk() {
+        this.name = "Bill"
         this.age = 18
+        this.profession = "Engineer"
+        this.badS = false
+    }
+}
+
+class Bob extends ksp_man {
+    talk() {
+        this.name = "Bob"
+        this.age = 21
+        this.profession = "Scientist"
+        this.badS = false
+    }
+}
+
+class Valentina extends ksp_man {
+    talk() {
+        this.name = "Valentina"
+        this.age = 23
+        this.profession = "pilot"
+        this.badS = false
     }
 }
