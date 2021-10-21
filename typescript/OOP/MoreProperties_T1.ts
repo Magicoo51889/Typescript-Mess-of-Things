3b589bd1225662718fdd841cf9382ea2bd96a8e0
Magicoo51889 <61296373+Magicoo51889@users.noreply.github.com>
2021-10-21 11:28:43 +0100

    Create MoreProperties_T1.ts

---
+++ typescript/OOP/MoreProperties_T1.ts
@@ -0,0 +1,30 @@
+interface Person {
+    name:string;
+    age:number;
+    profession:string;
+}
+
+const person1: Person = {
+    name: "Bob",
+    age: 56,
+    profession: "horseracer"
+}
+
+console.log(person1);
+
+const person2: Person = {
+    name: "Alice",
+    age: 34,
+    profession: "Engineer"
+}
+
+console.log(person2)
+
+const person3: Person = {
+    name: "David",
+    age: 18,
+    profession: "cobblesmith"
+}
+
+console.log(person3)
+
